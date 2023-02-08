50a2b9d277b095a6e52c87c762cbad0d7dbaba46
alli-Codes <allisonevans081@gmail.com>
2023-02-08 11:15:33 +0100

    Styling the modal component

--- components/Projects/modal.js
+++ components/Projects/modal.js
@@ -18,9 +18,9 @@ function Modal(props) {
       <div
         className={`overlay h-full w-full bg-[#00f97c14] px-5 flex justify-center md:items-center fixed z-[10000] ${overLayState}`}
       >
-        <div className="relative flex items-center">
-          <div className="modal bg-white-100 h-full h-[80vh]  flex flex-col lg:flex-row rounded-lg overflow-hidden relative">
-            <section className="bg-black h-full max-w-[40rem] image__section flex items-center">
+        <div className="relative flex items-center px-5 ">
+          <div className="modal bg-white-100 h-auto md:h-[80vh] flex flex-col lg:flex-row rounded-lg overflow-hidden relative">
+            <section className="bg-black h-full  w-full max-w-[15rem] lg:max-w-[40rem] image__section flex items-center overflow-hidden">
               <Swiper
                 className="h-full w-full"
                 slidesPerView={1}
@@ -36,7 +36,7 @@ function Modal(props) {
                       <section className="w-full h-full flex justify-center items-center ">
                         <img
                           src={`/images/projects/${image_url}`}
-                          className="h-full w-full object-cover"
+                          className="h-[15rem] md:h-full w-full object-cover"
                         />
                       </section>
                     </SwiperSlide>
@@ -45,21 +45,21 @@ function Modal(props) {
                 })}
               </Swiper>
             </section>
-            <section className="description__section max-w-[20rem] h-full py-10 px-5 flex flex-col gap-y-4 md:gap-y-10 overflow-y-auto break-words">
-              <h1 className="text-3xl md:text-3xl text-green-800 font-extrabold">
+            <section className="description__section max-w-[25rem] h-full py-10 px-5 flex flex-col gap-y-4 md:gap-y-10 overflow-y-auto break-words">
+              <h1 className="text-2xl md:text-3xl text-green-800 font-extrabold">
                 {project.title}
               </h1>
               <p className="text-justify ">{project.description}</p>
             </section>
+            <button
+              onClick={() => {
+                changeState();
+              }}
+              className="absolute top-2 right-2 z-[1000] bg-dark-100 text-white-100  w-[2rem] h-[2rem] flex items-center justify-center font-bold text-sm rounded-full md:right-3 md:top-3"
+            >
+              X
+            </button>
           </div>
-          <button
-            onClick={() => {
-              changeState();
-            }}
-            className="absolute top-24 right-4 z-[1000] bg-white-100  w-[2rem] h-[2rem] flex items-center justify-center font-bold text-sm rounded-full md:right-3 md:top-3 md:right-[-3rem] md:top-0"
-          >
-            X
-          </button>
         </div>
       </div>
     </>
