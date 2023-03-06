55105c0855d78c0099487f0a9ce3988bb4e0e2b9
alli-Codes <allisonevans081@gmail.com>
2023-03-06 15:49:37 +0100

    Adding all my emailjs keys and installing emailjs package

--- config/index.js
+++ config/index.js
@@ -1,9 +1,7 @@
-
-
 // EMAILJS Config Data
 
-export const EMAILJS_TEMPLATE_ID = ""
+export const EMAILJS_TEMPLATE_ID = "template_an3gdee";
 
-export const EMAILJS_SERVICE_ID = ""
+export const EMAILJS_SERVICE_ID = "service_rmqxpsu";
 
-export const EMAILJS_PUBLIC_KEY = ""
+export const EMAILJS_PUBLIC_KEY = "vx_EItOiBSJ84x5W8";
